c02da237676b1c1fe67a99515736f9fadc118a00
VeronikaBalter <veronika.balter@faceit-team.com>
2020-07-06 16:35:41 +0300

    save

---
+++ spa/src/components/layouts/BaseHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar
+      absolute
+      color="#fcb69f"
+      dark
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-toolbar-title>Start in IT</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      
+    </v-app-bar>
+</template>
+
+
+<script lang="ts">
+import { Component,Vue, Watch} from 'vue-property-decorator';
+@Component({
+})
+export default class BaseHeader extends Vue {
+}
+</script>
